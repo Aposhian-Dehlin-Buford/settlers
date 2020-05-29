ac12c003e80d5c69de574e312bf40f3f00753045
jondeh <dehlinjd@gmail.com>
2020-05-29 16:07:47 -0600

    styled myHand.js and Dice.js and added functionality

--- src/Components/MyHand.js
+++ src/Components/MyHand.js
@@ -5,14 +5,19 @@ const MyHand = () => {
   const { sheep, wood, clay, wheat, rock } = useSelector(
     ({ gameReducer }) => gameReducer.resources
   )
+
+  const newHand = () => {
+    let resArr = [sheep, wood, clay, wheat, rock]
+    let resHand = resArr.map((e,i) => [...Array(e)].map((f,j) => (i === 0 ? 'wheat' : i === 1 ? 'sheep' : i === 2 ? 'wood' : i === 3 ? 'rock' : 'clay'))).flat()
+    return resHand.map((e,i) => {
+      return <div className={e}>{e}</div>
+    })
+  }
+  let myHand = newHand()
+  
   return (
-    <div>
-      <div>My Hand</div>
-      <div>Sheep: {sheep}</div>
-      <div>Wood: {wood}</div>
-      <div>Clay: {clay}</div>
-      <div>Wheat: {wheat}</div>
-      <div>Rock: {rock}</div>
+    <div className="my-hand-container">
+      {myHand}
     </div>
   )
 }
