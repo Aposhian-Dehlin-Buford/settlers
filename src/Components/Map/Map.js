cc8be6dc068c0c03503fda96bde73305a308925d
jondeh <dehlinjd@gmail.com>
2020-05-29 12:54:39 -0600

    moved map to Map.js

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,9 +2,18 @@ import React, {useState, useEffect} from 'react';
 import './Map.scss';
 
 const Map = (props) => {
+
+    const {map} = props
+
+    const mappedMap = map.map((e,i) => e.map((f,j) => {
+        return <div className={i % 2 ? "hexagon" : "hexagon-odd"} style={{background: i % 2 === 0 && j === 8 ? 'black' : f.terrain === 'water' ? 'blue' : f.terrain === 'wheat' ? 'khaki' : f.terrain === 'sheep' ? 'green' : f.terrain === 'wood' ? 'darkgreen' : f.terrain === 'rock' ? 'grey' : f.terrain === 'clay' ? 'brown' : f.terrain === 'desert' ? 'tan' : f.terrain === 'port' ? 'radial-gradient(blue, black)' : 'blue'}}>{f.number ? <div className="number-container">{f.number}</div> : null}</div>
+    }))
+
+
+
     return (
         <div className="map-container">
-            Hello
+            {mappedMap}
         </div>
     )
 }
