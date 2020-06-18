a42b656787a3740bca57678e47ad916954ee374f
jondeh <dehlinjd@gmail.com>
2020-06-18 17:08:13 -0600

    added settlement placement for each player

--- server/utils/lobbyUtils.js
+++ server/utils/lobbyUtils.js
@@ -98,7 +98,7 @@ const generateInitialGameState = (
     // opponentsInfo: [
     //   { resources: { sheep: 3, wood: 3, clay: 3, wheat: 3, rock: 3 } },
     // ],
-    buildings: [],
+    buildings: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],
     developmentDeck: seedDeck(),
     developmentHand: [],
     map: seedMap(),
