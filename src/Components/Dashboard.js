689daa9367399f13c41a5f2e33b1248f56db9da8
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-07-02 21:00:07 -0600

    initial styling for login/userlist components

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,18 +3,23 @@ import { useSelector } from "react-redux"
 import useAuth from "../hooks/useAuth"
 import UserList from "./UserList"
 import ChallengeList from "./ChallengeList"
-import Game from './Game'
-import {UserContext} from '../context/UserContext'
+import Game from "./Game"
+import { UserContext } from "../context/UserContext"
+import "./Dashboard.scss"
 
 const Dashboard = () => {
   useAuth()
-  const {socket} = useContext(UserContext)
-  const {gameStart} = useSelector((redux) => redux)
+  const { socket } = useContext(UserContext)
+  const { gameStart } = useSelector((redux) => redux)
   return (
     <div>
-      {socket && !gameStart && <UserList />}
-      {socket && !gameStart && <ChallengeList/>}
       {socket && gameStart && <Game />}
+      {socket && !gameStart && (
+        <div className="list-container">
+          <UserList />
+          <ChallengeList />
+        </div>
+      )}
     </div>
   )
 }
